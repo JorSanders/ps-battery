c937af70f6d0d96b96feed565c29a955626c9d6b
Jor Sanders <it@jor.dev>
2025-10-18 21:05:48 +0200

    fix: dont wrap whole show balloon in unsafe

--- src/ps_battery/tray/show_balloon.rs
+++ src/ps_battery/tray/show_balloon.rs
@@ -16,24 +16,24 @@ pub struct ShowBalloonArgs<'a> {
 }
 
 pub fn show_balloon(args: &mut ShowBalloonArgs) {
+    let title_utf16: Vec<u16> = args.title.encode_utf16().chain(Some(0)).collect();
+    let msg_utf16: Vec<u16> = args.message.encode_utf16().chain(Some(0)).collect();
+
+    args.notify.uFlags |= NIF_INFO;
+
+    let msg_len = msg_utf16.len().min(args.notify.szInfo.len());
+    args.notify.szInfo[..msg_len].copy_from_slice(&msg_utf16[..msg_len]);
+
+    let title_len = title_utf16.len().min(args.notify.szInfoTitle.len());
+    args.notify.szInfoTitle[..title_len].copy_from_slice(&title_utf16[..title_len]);
+
+    args.notify.dwInfoFlags = match args.icon {
+        BalloonIcon::Info => NIIF_INFO,
+        BalloonIcon::Warning => NIIF_WARNING,
+        BalloonIcon::Error => NIIF_ERROR,
+    };
+
     unsafe {
-        let title_utf16: Vec<u16> = args.title.encode_utf16().chain(Some(0)).collect();
-        let msg_utf16: Vec<u16> = args.message.encode_utf16().chain(Some(0)).collect();
-
-        args.notify.uFlags |= NIF_INFO;
-
-        let msg_len = msg_utf16.len().min(args.notify.szInfo.len());
-        args.notify.szInfo[..msg_len].copy_from_slice(&msg_utf16[..msg_len]);
-
-        let title_len = title_utf16.len().min(args.notify.szInfoTitle.len());
-        args.notify.szInfoTitle[..title_len].copy_from_slice(&title_utf16[..title_len]);
-
-        args.notify.dwInfoFlags = match args.icon {
-            BalloonIcon::Info => NIIF_INFO,
-            BalloonIcon::Warning => NIIF_WARNING,
-            BalloonIcon::Error => NIIF_ERROR,
-        };
-
         let res = Shell_NotifyIconW(NIM_MODIFY, args.notify);
         if res.as_bool() {
             println!(
